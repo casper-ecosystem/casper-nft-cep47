8bf7b7a03eff316e621dcde982387d0d612d59d5
Maciej Zielinski <maciek.s.zielinski@gmail.com>
2021-05-25 16:26:34 +0200

    attach, detach wip

--- cep47/src/logic.rs
+++ cep47/src/logic.rs
@@ -107,12 +107,26 @@ trait CEP47Contract<Storage: CEP47Storage>: WithStorage<Storage> {
     }
 
     // URef releated function.
-    fn detach(&mut self, owner: PublicKey, token_id: TokenId) -> URef {
-        todo!();
+    fn detach(&mut self, owner: PublicKey, token_id: TokenId) -> Option<URef> {
+        let mut tokens = self.storage().get_tokens(owner);
+        if !tokens.contains(&token_id) {
+            None
+        } else {
+            tokens.retain(|x| x != &token_id);
+            self.storage_mut().set_tokens(owner, tokens);
+            self.storage_mut().new_uref(token_id)
+        }
     }
-    fn attach(&mut self, token_uref: URef, recipient: PublicKey) {}
+
+    fn attach(&mut self, token_uref: URef, recipient: PublicKey) {
+        let token_id = self.storage_mut().del_uref(token_uref).unwrap();
+        // load tokens of recipient
+        // add token to list
+        // save tokens
+    }
+    
     fn token_id(&self, token_uref: URef) -> TokenId {
-        todo!();
+        self.storage().token_id(token_uref).unwrap()
     }
 }
 
@@ -133,16 +147,21 @@ trait CEP47Storage {
     fn set_tokens(&mut self, owner: PublicKey, token_ids: Vec<TokenId>);
     fn mint_many(&mut self, recipient: PublicKey, token_uris: Vec<URI>);
     fn mint_copies(&mut self, recipient: PublicKey, token_uri: URI, count: U256);
-    fn new_uref(&mut self, token_id: TokenId) -> URef;
-    fn del_uref(&mut self, token_uref: URef);
+    
+    fn new_uref(&mut self, token_id: TokenId) -> Option<URef>;
+    fn del_uref(&mut self, token_uref: URef) -> Option<TokenId>;
+    fn token_id(&self, token_uref: URef) -> Option<TokenId>;
 }
 
 #[cfg(test)]
 mod tests {
+    use rand::Rng;
+    use types::AccessRights;
+
     use super::{
-        AsymmetricType, CEP47Contract, CEP47Storage, PublicKey, TokenId, WithStorage, U256, URI,
+        AsymmetricType, CEP47Contract, CEP47Storage, PublicKey, TokenId, WithStorage, U256, URI, URef
     };
-    use std::{collections::{hash_map::DefaultHasher, BTreeMap}, hash::{Hash, Hasher}, sync::Mutex};
+    use std::{collections::{BTreeMap, hash_map::DefaultHasher}, hash::{Hash, Hasher}, sync::Mutex};
 
     struct TestStorage {
         name: String,
@@ -153,6 +172,7 @@ mod tests {
         token_uris: BTreeMap<TokenId, URI>,
         balances: BTreeMap<PublicKey, U256>,
         belongs_to: BTreeMap<TokenId, PublicKey>,
+        urefs: BTreeMap<URef, TokenId>,
     }
 
     impl TestStorage {
@@ -166,6 +186,7 @@ mod tests {
                 balances: BTreeMap::new(),
                 belongs_to: BTreeMap::new(),
                 token_uris: BTreeMap::new(),
+                urefs: BTreeMap::new()
             }
         }
     }
@@ -275,12 +296,34 @@ mod tests {
             self.mint_many(recipient, token_uris);
         }
 
-        fn new_uref(&mut self, token_id: super::TokenId) -> types::URef {
-            todo!()
+        fn new_uref(&mut self, token_id: super::TokenId) -> Option<URef> {
+            let mut rng = rand::thread_rng();
+            let val: [u8; 32] = rng.gen();
+            let uref = URef::new(
+                val, 
+                AccessRights::READ_ADD_WRITE
+            );
+            if self.urefs.contains_key(&uref) {
+                None
+            } else {
+                self.urefs.insert(uref, token_id);
+                Some(uref)
+            }
         }
 
-        fn del_uref(&mut self, token_uref: types::URef) {
-            todo!()
+        fn del_uref(&mut self, token_uref: URef) -> Option<TokenId> {
+            let token_id = self.token_id(token_uref);
+            if token_id.is_none() {
+                None
+            } else {
+                let token_id = token_id.unwrap();
+                self.urefs.remove(&token_uref);
+                Some(token_id)
+            }
+        }
+
+        fn token_id(&self, token_uref: URef) -> Option<TokenId> {
+            self.urefs.get(&token_uref).map(|x| x.clone())
         }
     }
 
@@ -431,6 +474,7 @@ mod tests {
         let owner_of_second_token_id = contract.owner_of(ali_second_token_id);
         assert_eq!(owner_of_second_token_id.unwrap(), bob);
     }
+    
     #[test]
     fn test_transfer_many_tokens() {
         let mut contract = TestContract::new();
@@ -470,5 +514,28 @@ mod tests {
         let owner_of_third_token_id = contract.owner_of(ali_third_token_id);
         assert_eq!(owner_of_second_token_id.unwrap(), bob);
         assert_eq!(owner_of_third_token_id.unwrap(), bob);
+    }
+
+    #[test]
+    fn test_attach_and_detach() {
+        let mut contract = TestContract::new();
+        let ali = PublicKey::ed25519_from_bytes([3u8; 32]).unwrap();
+        let bob = PublicKey::ed25519_from_bytes([6u8; 32]).unwrap();
+
+        contract.mint_one(ali, URI::from("0x12af"));
+        let token_id: TokenId = contract.tokens(ali)[0].clone();
+        
+        let token_uref: URef = contract.detach(ali, token_id.clone()).unwrap();
+        assert_eq!(contract.balance_of(ali), U256::zero());
+        assert_eq!(contract.total_supply(), U256::one());
+        assert!(contract.tokens(ali).is_empty());
+
+        assert_eq!(contract.token_id(token_uref.clone()), token_id.clone());
+        assert_eq!(contract.token_uri(token_id.clone()).unwrap(), URI::from("0x12af"));
+
+        contract.attach(token_uref, bob);
+        assert_eq!(contract.balance_of(bob), U256::one());
+        assert_eq!(contract.total_supply(), U256::one());
+        assert_eq!(contract.tokens(bob), vec![token_id]);
     }
 }

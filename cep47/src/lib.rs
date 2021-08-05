a94d96580bdc8ce6d4d17a7653eec3ebda1bef93
Maciej Zielinski <maciek.s.zielinski@gmail.com>
2021-08-05 13:06:52 +0200

    get_caller

--- cep47/src/lib.rs
+++ cep47/src/lib.rs
@@ -281,8 +281,18 @@ pub fn ret<T: CLTyped + ToBytes>(value: T) {
     runtime::ret(CLValue::from_t(value).unwrap_or_revert())
 }
 
-fn get_caller() -> Key {
-    match runtime::get_call_stack().first().unwrap_or_revert() {
+pub fn get_caller() -> Key {
+    let call_stack = runtime::get_call_stack();
+    let caller = call_stack.get(call_stack.len() - 2);
+    element_to_key(caller.unwrap_or_revert())
+}
+
+pub fn self_addr() -> Key {
+    element_to_key(runtime::get_call_stack().last().unwrap_or_revert())
+}
+
+fn element_to_key(element: &CallStackElement) -> Key {
+    match element {
         CallStackElement::Session { account_hash } => (*account_hash).into(),
         CallStackElement::StoredSession {
             account_hash,
@@ -292,6 +302,6 @@ fn get_caller() -> Key {
         CallStackElement::StoredContract {
             contract_package_hash,
             contract_hash: _,
-        } => (*contract_package_hash).into(),
+        } => (*contract_package_hash).into()
     }
 }

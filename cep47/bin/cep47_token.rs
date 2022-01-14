0bc360cb796c0d57aeb89703aabd721c71458bd4
astro019 <dmitry.yakov7@gmail.com>
2022-01-14 23:56:34 +1000

    update contract package initialization

--- cep47/bin/cep47_token.rs
+++ cep47/bin/cep47_token.rs
@@ -4,19 +4,13 @@
 #[macro_use]
 extern crate alloc;
 
-use alloc::{
-    boxed::Box,
-    collections::BTreeSet,
-    format,
-    string::{String, ToString},
-    vec::Vec,
-};
+use alloc::{boxed::Box, collections::BTreeSet, format, string::String, vec::Vec};
 use casper_contract::{
     contract_api::{runtime, storage},
     unwrap_or_revert::UnwrapOrRevert,
 };
 use casper_types::{
-    contracts::NamedKeys, runtime_args, CLType, CLTyped, CLValue, EntryPoint, EntryPointAccess,
+    runtime_args, CLType, CLTyped, CLValue, ContractPackageHash, EntryPoint, EntryPointAccess,
     EntryPointType, EntryPoints, Group, Key, Parameter, RuntimeArgs, URef, U256,
 };
 use cep47::{Meta, TokenId, CEP47};
@@ -176,20 +170,11 @@ fn get_approved() {
 
 #[no_mangle]
 fn call() {
-    let (package_hash, access_token) = storage::create_contract_package_at_hash();
-    let mut named_keys = NamedKeys::new();
-    let contract_package_hash_wrapped = storage::new_uref(package_hash).into();
-    named_keys.insert(
-        "contract_package_hash".to_string(),
-        contract_package_hash_wrapped,
-    );
-    let (contract_hash, _) =
-        storage::add_contract_version(package_hash, get_entry_points(), named_keys);
-
     // Read arguments for the constructor call.
     let name: String = runtime::get_named_arg("name");
     let symbol: String = runtime::get_named_arg("symbol");
     let meta: Meta = runtime::get_named_arg("meta");
+    let contract_name: String = runtime::get_named_arg("contract_name");
 
     // Prepare constructor args
     let constructor_args = runtime_args! {
@@ -198,33 +183,33 @@ fn call() {
         "meta" => meta
     };
 
-    // Add the constructor group to the package hash with a single URef.
+    let (contract_hash, _) = storage::new_contract(
+        get_entry_points(),
+        None,
+        Some(String::from("contract_package_hash")),
+        None,
+    );
+
+    let package_hash: ContractPackageHash = ContractPackageHash::new(
+        runtime::get_key("contract_package_hash")
+            .unwrap_or_revert()
+            .into_hash()
+            .unwrap_or_revert(),
+    );
+
     let constructor_access: URef =
         storage::create_contract_user_group(package_hash, "constructor", 1, Default::default())
             .unwrap_or_revert()
             .pop()
             .unwrap_or_revert();
 
-    // Call the constructor entry point
-    let _: () =
-        runtime::call_versioned_contract(package_hash, None, "constructor", constructor_args);
+    let _: () = runtime::call_contract(contract_hash, "constructor", constructor_args);
 
-    // Remove all URefs from the constructor group, so no one can call it for the second time.
     let mut urefs = BTreeSet::new();
     urefs.insert(constructor_access);
     storage::remove_contract_user_group_urefs(package_hash, "constructor", urefs)
         .unwrap_or_revert();
 
-    // Store contract in the account's named keys.
-    let contract_name: alloc::string::String = runtime::get_named_arg("contract_name");
-    runtime::put_key(
-        &format!("{}_package_hash", contract_name),
-        package_hash.into(),
-    );
-    runtime::put_key(
-        &format!("{}_package_hash_wrapped", contract_name),
-        contract_package_hash_wrapped,
-    );
     runtime::put_key(
         &format!("{}_contract_hash", contract_name),
         contract_hash.into(),
@@ -232,10 +217,6 @@ fn call() {
     runtime::put_key(
         &format!("{}_contract_hash_wrapped", contract_name),
         storage::new_uref(contract_hash).into(),
-    );
-    runtime::put_key(
-        &format!("{}_package_access_token", contract_name),
-        access_token.into(),
     );
 }
 
